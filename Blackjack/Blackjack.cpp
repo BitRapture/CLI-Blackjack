d6964dc5d6721c416b91850ac85a96aff289bdb6
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-01-20 03:45:56 +0000

    Add debug testing

--- Blackjack/Blackjack.cpp
+++ Blackjack/Blackjack.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
+#include "Cards.h"
 
 int main()
 {
+	Card* g_deck;
+	bool g_errorHandling = false;
 
+	g_errorHandling = !InitialiseCardDeck(g_deck);
+
+	if (g_errorHandling)
+	{
+		std::cout << "There was an error!";
+	}
+	else
+	{
+		PrintCardDeck(g_deck);
+	}
 }
