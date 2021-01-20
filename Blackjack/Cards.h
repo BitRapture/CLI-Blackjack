d8ae7391213504b176c0daec342aac55a8c5c2d2
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-01-20 03:45:40 +0000

    Add function declarations

--- Blackjack/Cards.h
+++ Blackjack/Cards.h
@@ -1,14 +1,20 @@
 /// \file Cards.h
-/// \brief encapsulates a 4d Homogenous Point / Vector object
+/// \brief Contains a card structure and related functions
 /// \author George Smith
 
 #ifndef _CARDS_H_
 #define _CARDS_H_
 
-//File Includes
-
-
 //Header Contents
+struct Card
+{
+	int m_cardValue{ 0 };
+	char m_cardSuit[10]{ 0 };
+	char m_cardName[10]{ 0 };
+};
 
+bool InitialiseCardDeck(Card*& _deck);
 
-#endif    // _CARDS_H_
+void PrintCardDeck(Card*& _deck);
+
+#endif  // _CARDS_H_
