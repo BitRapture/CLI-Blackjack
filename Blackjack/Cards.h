ef4cfdfa8dd226c7dfd2b59690596b906591bcce
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-01-20 00:33:24 +0000

    Add project files.

---
+++ Blackjack/Cards.h
@@ -0,0 +1,14 @@
+/// \file Cards.h
+/// \brief encapsulates a 4d Homogenous Point / Vector object
+/// \author George Smith
+
+#ifndef _CARDS_H_
+#define _CARDS_H_
+
+//File Includes
+
+
+//Header Contents
+
+
+#endif    // _CARDS_H_
